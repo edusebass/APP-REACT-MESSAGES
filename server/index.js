d29b82d745a67b927c87628d03908ac910a93ae7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-25 14:06:31 -0500

    vercel prueba

--- server/index.js
+++ server/index.js
@@ -1,45 +1,29 @@
 import express from 'express';
-import logger from 'morgan';
-
+import http from 'http';
 import { Server } from 'socket.io';
-import { createServer } from 'node:http';
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const port = process.env.PORT ?? 3000;
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'client')));
-app.set("client", path.join(__dirname, "client"));
-app.set("view engine", "html");
+app.use(express.static(path.join(process.cwd(), 'client')));
 
 io.on('connection', (socket) => {
-    console.log('a user coneccted');
+    console.log('a user connected');
 
-    socket.on('disconnect', () =>{
-        console.log('an user disconnected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
     });
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
         console.log('chat message: ' + msg);
     });
 });
 
-app.use(logger('dev'))
-
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
+    res.sendFile(path.join(process.cwd(), 'client', 'index.html'));
 });
 
-server.listen(port, () => { 
-    console.log('Port running on port ' + port);
-});
+export default server;

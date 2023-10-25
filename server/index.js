f61f4115c9fc172287cd5baa72ad2a72d53f9a69
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-25 13:59:57 -0500

    prueba vercel

--- server/index.js
+++ server/index.js
@@ -4,12 +4,23 @@ import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const port = process.env.PORT ?? 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     connectionStateRecovery: {}
 });
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.set("client", path.join(__dirname, "client"));
+app.set("view engine", "html");
 
 io.on('connection', (socket) => {
     console.log('a user coneccted');

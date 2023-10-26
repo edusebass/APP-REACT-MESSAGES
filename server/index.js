5ffc83dc26a93e5970da8a6754e184cf3a1e6e2b
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-25 19:59:35 -0500

    prueba netlify

--- server/index.js
+++ server/index.js
@@ -1,44 +1,34 @@
-import express from 'express';
-import logger from 'morgan';
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-import { Server } from 'socket.io';
-import { createServer } from 'node:http';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const port = process.env.PORT ?? 3000;
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(logger('dev'))
 app.use(express.static(path.resolve(__dirname, '../client')));
 
 io.on('connection', (socket) => {
-    console.log('a user coneccted');
+    console.log('a user connected');
 
-    socket.on('disconnect', () =>{
-        console.log('an user disconnected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
         console.log('chat message: ' + msg);
     });
 });
 
+// Ruta para servir el archivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => { 
-    console.log('Port running on port ' + port);
-    console.log('directiorio: ' + __dirname + 'nombre: ' +  __filename)
+server.listen(port, () => {
+    console.log('Server running on port ' + port);
 });

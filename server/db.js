3cbf039e922e6b025d957c9be0fbf08b9365edcb
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-27 13:36:48 -0500

    importacion de variable de config del .env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 import { connect } from 'mongoose';
+import { MONGODB_URI } from './config.js';
 
 export const connectDB = async () => {
     try {
-        await connect('mongodb://127.0.0.1:27017/app-mensajes')
+        await connect(MONGODB_URI)
         console.log('Connect to DB')
     } catch (error) {
         console.error(error)

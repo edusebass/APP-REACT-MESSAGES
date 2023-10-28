713fc4a31352438c1bae18887804ddd221f67140
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-27 22:28:52 -0500

    ubicacion para el nombre y const para la creacion del nombre

--- client/main.js
+++ client/main.js
@@ -2,26 +2,29 @@ import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
 const socket = io();
 
 const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const inputMessage = document.getElementById('input');
+const nombreInput = document.getElementById('nombreInput');
 
 socket.on('initial messages', (msgs) => {
     msgs.forEach(msg => {
-        const item = `<li>${msg.msg}</li>`;
+        const item = `<li> <span>${msg.nombre}</span> <br> ${msg.msg}</li>`;
         messages.insertAdjacentHTML('beforeend', item);
     });
 });
 
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
+    const nombre = nombreInput.value;
+    const message = inputMessage.value;
+    if (message && nombre) {
+        socket.emit('chat message', { messageNombre: nombre, message });
+        nombreInput.value = '';
+        inputMessage.value = '';
     }
 });
 
 socket.on('chat message', (msg) => {
-    const item = `<li>${msg}</li>`;
+    const item = `<li> <span>${msg.messageNombre}</span> <br> ${msg.message}</li>`;
     messages.insertAdjacentHTML('beforeend', item);
 });
